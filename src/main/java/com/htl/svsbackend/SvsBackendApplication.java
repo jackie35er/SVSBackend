a7561a887339941482b1b9f58355c6c1d64980c8
jackie35er <jackie35er@gmail.com>
2022-03-07 11:17:49 +0100

    Initial commit

---
+++ src/main/java/com/htl/svsbackend/SvsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.htl.svsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SvsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/htl/svsbackend/SvsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htl.svsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
